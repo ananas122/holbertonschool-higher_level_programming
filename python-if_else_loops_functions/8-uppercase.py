52a696dc9826d4a4e4bf06c5fc5d21b8545b8771
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-05-30 15:44:49 +0200

    uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 def uppercase(str):
-    
+    uppercase = 0
+    for i in str:
+        if(ord(i) >= 97 and ord(i) <= 122):
+            uppercase = 32
+        else:
+            uppercase = 0
+        print("{}".format(chr(ord(i) - uppercase)), end='')
+    print()
+
+#!/usr/bin/env python3
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
