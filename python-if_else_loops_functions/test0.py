539ab7b5fb25d0c3a168c15dfdcdfbb7b2702939
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-05-30 15:12:52 +0200

    main.py

---
+++ python-if_else_loops_functions/test0.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if number > 0:
+    print("{} is positive".format(number))
+elif number == 0:
+    print("{} is zero".format(number))
+else:
+    print("{} is negative".format(number))
