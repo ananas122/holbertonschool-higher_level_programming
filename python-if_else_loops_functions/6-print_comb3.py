4b43e801fcc472c146df265a2b2c8d2f6c7248df
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-05-30 19:47:24 +0200

    Correction pycodestyle to delete lines and spaces

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for dizaine in range(0, 9): # Parcours les dizaines de 0 à 8
-    for unite in range(dizaine + 1, 10): # Parcours les unités de dizaine + 1 à 9
-        if dizaine == 8 and unite == 9: #Verifie le dernier nombre 
+for dizaine in range(0, 9):  # Parcours les diz de 0 à 8
+    for unite in range(dizaine + 1, 10):  # Parcours les unités de diz+1 à 9
+        if dizaine == 8 and unite == 9:  # Verifie le dernier nombre
             print("{:d}{:d}".format(dizaine, unite))
         else:
-            print("{:d}{:d}".format(dizaine, unite), end=", ")#Sinon on imprime des nombres avc ", "
+            print("{:d}{:d}".format(dizaine, unite), end=", ")
+            # Sinon print des nbr avc ", "
