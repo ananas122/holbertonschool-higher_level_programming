8cf8cbe815565beffde52e7edaa1ff844be23f4b
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-05-30 15:48:25 +0200

    Correction pycodestyle delete spaces

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,15 +2,9 @@
 def uppercase(str):
     uppercase = 0
     for i in str:
-        if(ord(i) >= 97 and ord(i) <= 122):
+        if (ord(i) >= 97 and ord(i) <= 122):
             uppercase = 32
         else:
             uppercase = 0
         print("{}".format(chr(ord(i) - uppercase)), end='')
     print()
-
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
