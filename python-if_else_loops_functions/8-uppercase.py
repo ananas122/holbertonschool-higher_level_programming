261851045f379216be4919700f18e6045f13f78a
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-05-30 15:59:47 +0200

    remplacer le nom de la variable

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     uppercase = 0
-    for i in str:
-        if (ord(i) >= 97 and ord(i) <= 122):
+    for alphabet in str:
+        if (ord(alphabet) >= 97 and ord(alphabet) <= 122):
             uppercase = 32
         else:
             uppercase = 0
-        print("{}".format(chr(ord(i) - uppercase)), end='')
+        print("{}".format(chr(ord(alphabet) - uppercase)), end='')
     print()
