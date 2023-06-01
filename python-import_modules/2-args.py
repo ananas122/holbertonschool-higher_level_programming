aaa5c68ff59281a713b2ab5c57d478d03c80714a
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-01 12:02:07 +0200

    Write a program that prints the number of and the list and arg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ == '__main__':
-    # print(sys.argv)
-    arguments = sys.argv[1:]
-    # print(arguments)
-    num_arguments = len(sys.argv) - 1
-#    print(num_arguments)
-    if num_arguments == 0:
+if __name__ == "__main__":
+    import sys
+    # Afficher le nombre d'arguments
+    arguments = len(sys.argv) - 1
+    if arguments == 0:
         print("0 arguments.")
-    elif num_arguments == 1:
-        print("1 argument")
-        print(f"1: {arguments[0]}")
-
+    elif arguments == 1:
+        print("1 argument:")
     else:
-        print(f"{num_arguments} arguments:")
-        for i in range(num_arguments):
-            print(f"{i}: {arguments[i]}")
+        print("{} arguments:".format(arguments))
+    for count in range(arguments):
+        # Afficher chaque argument avec son numéro
+        print("{}: {}".format(count + 1, sys.argv[count + 1]))
