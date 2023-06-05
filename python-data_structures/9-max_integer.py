976d2af40634cf45ffe6690625e3735be1c87f52
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-05 14:39:16 +0200

    Corrected to initialisation

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -7,9 +7,8 @@ def max_integer(my_list=[]):
     if my_list == 0:
         return None
 #  initialize to 0 for to get the first number in the list
-    max_value = 0
+    max_value = my_list[0] 
     for i in my_list:
         if i > max_value:
             max_value = i
-
     return max_value
