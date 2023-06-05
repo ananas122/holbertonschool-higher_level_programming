c703c3bb268d7d55ebd621a00321403db031c3aa
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-05 12:59:58 +0200

    Coorection pycodestyle to delete line

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -6,11 +6,10 @@ def max_integer(my_list=[]):
     # If the list is empty, return None
     if my_list == 0:
         return None
-
-    max_num = 0  #  initialize max_num to 0 for to get the first number in the list 
+#  initialize to 0 for to get the first number in the list
+    max_num = 0
     for i in my_list:
         if i > max_num:
             max_num = i
 
     return max_num
-
