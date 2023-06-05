567b344a76d92578a07fa613966e1a2c55b66d0d
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-05 17:32:34 +0200

    Correction to added len my list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,10 +4,11 @@
 
 def max_integer(my_list=[]):
     # If the list is empty, return None
-    if my_list == 0:
+    if len(my_list) == 0:
         return None
+    else:
 #  initialize to 0 for to get the first number in the list
-    max = my_list[0]
+        max = my_list[0]
     for i in my_list:
         if i > max:
             max = i
