7ed732d91b400ee2280c5b52c9b8ae274c328835
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-05 02:12:33 +0200

    Correction function that switch the value

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-# switch a and b
-a, b = b, a
-print(a, b)  # 10
-print(b, a)  # 89
+a, b = b, a  # switch between a and b
 print("a={:d} - b={:d}".format(a, b))
+
