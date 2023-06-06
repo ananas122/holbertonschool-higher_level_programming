75e7a7979bed554384c86db9a2c156be15e7226c
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-06 14:55:55 +0200

    Correction to delete line for picodestyle

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Write a function that returns the number of keys in a dictionary"""
+"""Write a function that returns the numb of keys in a dictionary"""
 
 
 def number_keys(a_dictionary):
     return len(a_dictionary)  # return the number of keys in a dictionary
-
