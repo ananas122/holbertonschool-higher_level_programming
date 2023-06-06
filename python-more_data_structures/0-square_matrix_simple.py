f11f8a8370f80866804c32b92861704271ac4825
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-06 12:45:33 +0200

    Write a function that computes the square val of all int of a matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 """Write a function that computes the square val of all int of a matrix"""
-
-
 def square_matrix_simple(matrix=[]):
     return [list(map(lambda x: x**2, row)) for row in matrix]
