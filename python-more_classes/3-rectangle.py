9485573784104f4ecae19abd397f2e03e70bdb42
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-13 18:22:12 +0200

    Correction

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,28 +1,52 @@
 #!/usr/bin/python3
-"""that defines a rectangle by: (based on 2-rectangle.py)"""
+"""
+Module creates the Rectangle class
+"""
 
 
 class Rectangle:
-    """Represent a rectangle"""
+    """
+    Class Rectangle with validated private instance attributes width and height
+    """
 
     def __init__(self, width=0, height=0):
-        """Initialize a new rectangle.
+        """Instantiates width and height using property setter
+
         Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+            width: width of rectangle
+            height: height of rectangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        """Returns a string of Rectangle instance using #, empty string"""
+        if self.width == 0 or self.height == 0:
+            return ""
+        row = "#" * self.width
+        rect = row
+        for i in range(self.height - 1):
+            rect += "\n" + row
+        return rect
 
     @property
     def width(self):
-        """Get the current width of the rectangle."""
+        """width: width of rectangle
+
+        setter validates value is an integer >= 0
+
+        Parameter:
+            value: value of the width
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is < 0
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -30,35 +54,33 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get the current height of the rectangle."""
+        """height: height of rectangle
+
+        setter validates value is an integer >= 0
+
+        Parameter:
+            value: value of the height
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is < 0
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
+        """Returns the calculated area of Rectangle instance"""
         return self.width * self.height
 
     def perimeter(self):
+        """Returns the calculated perimeter of Rectangle instance"""
         if self.height == 0 or self.width == 0:
             return 0
         return (self.width + self.height) * 2
-
-    def __str__(self):
-        """Returns a str using #, empty string"""
-        if self.width == 0 or self.height == 0:
-            return ""
-        #  Crée une ligne horiz
-        row = "#" * self.width
-        #  initialise le string avec la ligne horiz
-        rect = row
-        # parcours et ajoute les lignes horizontales
-        for _ in range(self.height):
-            rect += "\n" + row
-        return rect
