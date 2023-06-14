87d1e1d32266764c53dbaaac662f30e8e5bb86e9
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-14 16:32:07 +0200

    add int and test driven

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,8 +4,7 @@
 
 def add_integer(a, b=98):
     """Return the sum of a and b."""
-    if not isinstance(a, (int, float)) or not isinstance(b, (int,float)):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
 
-    return a + b
-
+    return int(a) + int(b)
