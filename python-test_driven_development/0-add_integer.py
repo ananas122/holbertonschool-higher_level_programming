c05ec76a7532d721e2ce949d58d1ace51fea05a4
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-15 10:38:33 +0200

    Correction test driven

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-""" Module 0 """
+""" Write a function that adds 2 integers."""
 
 
 def add_integer(a, b=98):
-    """Return the sum of a and b."""
+    """
+     Verify is they are int or float
+    """
+
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
 
-    return int(a) + int(b)
+    return int(a + b)
+
