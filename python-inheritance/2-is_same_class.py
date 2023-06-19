d4a68cf825fe3c81f27ff8251325e951d8b0e85b
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-19 11:43:57 +0200

    true and false condition for to return objet

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Function that returns True if the object is exactly an instance of the specified class ; otherwise False. """
-
 
 def is_same_class(obj, a_class):
+    """
+    Returns True if the object is exactly an instance of the specified class
+    otherwise False
+    """
+
     if object == a_class:
         return True
     else:
