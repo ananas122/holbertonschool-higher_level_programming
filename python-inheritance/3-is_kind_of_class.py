e1a54f5b98a7e039520e1439979b14f2e3a04e31
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-19 16:14:17 +0200

    Verification de l objet

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-""" Write a function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; 
-    otherwise False.
-Prototype: def is_kind_of_class(obj, a_class):"""
+""" Module for verification of instance """
 
 
 def is_kind_of_class(obj, a_class):
-    return isinstance(obj, a_class)
+    return (isinstance(obj, a_class))
