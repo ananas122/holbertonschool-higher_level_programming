d9f6591113d77d41bbac998da6537e788acefe5e
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-19 16:56:39 +0200

    Returns True or false if obj inherits from a_class or not

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,5 @@ Module for Verification
 """
 
 def inherits_from(obj, a_class):
-    return (issubclass(obj, a_class) and type(obj) != a_class)
+    """Returns True or false if obj inherits from a_class or not"""
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
