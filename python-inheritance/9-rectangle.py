b7d798b3ac420c1bb2c13be79540d10483223646
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-19 19:19:14 +0200

    print and str

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    class Rectangle that inherits from BaseGeometry
+    based on(8-base_geometry.py).
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ rectangle class inherits from basegeometry """
+
+    def __init__(self, width, height):
+        """
+        Initializes a rectangle
+        """
+        super().integer_validator("height", height)
+        super().integer_validator("width", width)
+        self.__height = height
+        self.__width = width
+
+
+    def area(self):
+        """ returns the area of the rectangle """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ returns [Rectangle] <width>/<height> string """
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+  
+   
+    
+    
