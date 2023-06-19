42888ad9410a9fc1ce6fdfa385212d3bf50dc5fe
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-19 16:15:04 +0200

    Verification de l objet

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-""" Module for verification of instance """
+"""
+Module for task 3
+"""
 
 
 def is_kind_of_class(obj, a_class):
+    """Return a instance of a class"""
     return (isinstance(obj, a_class))
