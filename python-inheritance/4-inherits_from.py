212026da643f6217fd165fc65bc5fcab4c19686b
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-19 16:59:46 +0200

    Pycodestyle correction

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,6 +3,7 @@
 Module for Verification
 """
 
+
 def inherits_from(obj, a_class):
     """Returns True or false if obj inherits from a_class or not"""
     return (issubclass(type(obj), a_class) and type(obj) != a_class)
