7d7ed861b805790427a4d0c39c50594b5df0f276
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-20 15:09:34 +0200

    Write a function that creates an Obj from a JSON file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -4,5 +4,7 @@ import json
 
 
 def load_from_json_file(filename):
+    """ Read a JSON file and return Objet """
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
+
