fd9c6ba95232cf70c0eb0a2f101ad5e0fa281bb3
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-20 12:10:21 +0200

    Write a JSON representation

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Function that writes an Object to a text file,
+using a JSON representation """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Write a JSON representation """
+    with open('data.json', "w") as filename:
+        json.dump(my_obj, filename)
