8f78ada8095a91bec14a528d0922e78a281f2319
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-20 02:42:54 +0200

    Write and return function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,6 +2,8 @@
 """Write a function that writes a string to a text file (UTF8)
 and returns the number of characters written"""
 
+
 def write_file(filename="", text=""):
+    """Write a str to a text file and returns the number of characters"""
     with open(filename, mode="w", encoding="utf-8") as file:
         return (file.write(text))
