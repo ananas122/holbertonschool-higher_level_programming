cc283c973d432ef63f1feaaedd914322b055c873
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-20 02:22:54 +0200

    Function that reads a text file and print

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Write a func that reads a text file and prints it to stdout"""
+"""func that reads a text file and prints it to stdout"""
 
 
 def read_file(filename=""):
-""" Read a file and prints it to stdout"""
+    """ Read a file and prints it to stdout"""
     with open(filename, mode="r", encoding="utf-8") as file:
         print(file.read(), end="")
 
