061175515bb5c5a78fc770d6c0681c8043b2a3b0
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-20 19:30:27 +0200

    test driven

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Write a class MyList that inherits from list:"""
+"""Write a class MyList that inherits from list"""
 
 
 class MyList(list):
+    """ List of MyList instances """
+
     def print_sorted(self):
+
         """ Print the list as a sorted list """
         print(sorted(self))
