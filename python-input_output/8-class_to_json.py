bfe4576d64ed95e6461e465ed21bb6a00bccba3a
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-20 17:31:22 +0200

    Convert obj on dict

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-"""
-Function that returns the dict description
-with simple data structure (list, dictionary, string, integer and boolean)
-for JSON serialization of an object
-"""
-
-import json
+""" Function to return dictionary description for JSON serialization"""
 
 
 def class_to_json(obj):
-    """ Return a JSON representation of a class """
+    """returns dictionary description for JSON serialization"""
     return obj.__dict__
