ee7f1e6593616771df9cb0d17eea61f2476f6c26
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-21 17:26:58 +0200

    Correction to add json.dump

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-""" This function is used to generate the output of the command """
+""" Function that returns a list of strings representing the"""
 import json
 
 
 class Student:
-    """ Defines a class student."""
+    """A simple Student Class """
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
+
+
     def to_json(self, attrs=None):
-   
-        if attrs is None or type(attrs) is not list:
-            return self.__dict__
+        if attrs is None:
+            return json.dumps(self.__dict__)
+
+        if not isinstance(attrs, list):
+            return json.dumps(self.__dict__)
 
         new_dict = {}
-        if not all(isinstance(attr, str) for attr in attrs):
-            return self.__dict__
-        else:
-            for key, value in self.__dict__.items():
-                if key in attrs:
-                    new_dict[key] = value
-        return new_dict
+        for attr in attrs:
+            if not isinstance(attr, str):
+                return json.dumps(self.__dict__)
 
+            if attr in self.__dict__:
+                new_dict[attr] = self.__dict__[attr]
+
+        return json.dumps(new_dict)
