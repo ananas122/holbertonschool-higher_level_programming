7f621599b16368188a2fcdc79f2c3296314e5f15
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-21 17:32:00 +0200

    ajout des fonctions

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-""" Function that returns a list of strings representing the"""
 import json
 
 
 class Student:
-    """A simple Student Class """
+    """Defines a student."""
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-
-
     def to_json(self, attrs=None):
-         if attrs is None or type(attrs) is not list:
-            return self.__dict__
-
-        new_dict = {}
-        for attr in attrs:
-            if type(attr) is not str:
-                return self.__dict__
-            else:
-                if attr in self.__dict__:
-                    new_dict[attr] = self.__dict__[attr]
-        return new_dict
+        """Retrieves a dictionary representation of a Student instance."""
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
