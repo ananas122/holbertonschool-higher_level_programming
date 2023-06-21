c92d0c48677ed28d00fb4088b5382e4686d707d8
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-21 17:41:07 +0200

    ajout des fonctions

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import json
+"""student class"""
 
 
 class Student:
-    """Defines a student."""
-
+    """class stuff"""
     def __init__(self, first_name, last_name, age):
+        """initializes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        """prints __dict__"""
+        if(isinstance(attrs, list) and all(isinstance(x, str) for x in attrs)):
+            return({x: y for x, y in self.__dict__.items() if x in attrs})
         else:
             return self.__dict__
