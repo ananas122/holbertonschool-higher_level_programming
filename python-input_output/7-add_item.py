7818c2c383edc7af8d9623f635eead82678c06fd
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-22 11:16:52 +0200

    add items

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,9 +4,10 @@ imports the "save_to_json_file" and "load_from_json_file" functions from their r
     attempts to load a list from a file named "add_item.json", and creates an empty list if it does not exist.
     The script then adds all command-line arguments starting from the second one to the list,
     and saves the updated list back to the file using the "save_to_json_file" function.
-    """
+"""
 import json
 import sys
+
 
 # Retrieve all arguments passed to the script
 args = sys.argv[1:]
