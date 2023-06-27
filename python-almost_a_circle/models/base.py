70e2e1778fe437b12c5441d07bf462c31e309b8a
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-27 16:21:48 +0200

    Return json loads

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ Converts a list of dictionaries """
-        if list_dictionaries is None or list_dictionaries is []:
+        if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
 
@@ -27,13 +27,9 @@ class Base:
         filename = cls.__name__ + ".json"
         with open(filename, mode="w", encoding="utf-8") as file:
             json.dump([obj.to_dictionary() for obj in list_objs], file)
-    
+
     def from_json_string(json_string):
-       
-        """json_string is a string representing a list of dictionaries
-        If json_string is None or empty, return an empty list
-        Otherwise, return the list represented by json_string"""
-        if json_string is None or json_string is []:
+        """Create a new instance of the class from a json string """
+        if not json_string:
             return "[]"
         return json.loads(json_string)
-        
