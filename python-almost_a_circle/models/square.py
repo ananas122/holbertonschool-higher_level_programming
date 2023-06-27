d1042c969838389a4287f75795bba584081cd8e3
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-27 10:50:50 +0200

    propriety setter getter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,3 +13,14 @@ class Square(Rectangle):
     def __str__(self):
         """Define a string representation of a Rectangle obj """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """ Get the size of the Square """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Set the size of the Square """
+        self.width = value
+        self.height = value
