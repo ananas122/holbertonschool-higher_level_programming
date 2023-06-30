28366a3d8fea76a18eecf963ad18d06084860ee8
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-30 15:12:45 +0200

    test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,6 +4,8 @@ Defines unittests for models/base.py.
 """
 import os
 import unittest
+import sys
+import io
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
