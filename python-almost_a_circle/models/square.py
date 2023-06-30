2e24b7acd7095925cb5e365d20882c1a998d2c8f
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-06-30 15:55:13 +0200

    test

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,73 +1,58 @@
 #!/usr/bin/python3
-""" Module that contains class Square,
-inheritance of class Rectangle
-"""
+""" module Square that inherits from Rectangle:"""
+from models.base import Base
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Class Rectangle """
+    """ Class a Square that inherites by Rectangle. """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initializes instances """
+        # Call the superclass constructor to initialize the obj
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """ str special method """
-        str_square = "[Square] "
-        str_id = "({}) ".format(self.id)
-        str_xy = "{}/{} - ".format(self.x, self.y)
-        str_wh = "{}/{}".format(self.width, self.height)
-
-        return str_square + str_id + str_xy + str_wh
+        """Define a string representation of a Rectangle obj """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
-        """ Getter size """
+        """ Get the size of the Square """
         return self.width
 
     @size.setter
     def size(self, value):
-        """ Setter size """
+        """ Set the size of the Square """
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """ str special method """
-        str_rectangle = "[Square] "
-        str_id = "({}) ".format(self.id)
-        str_xy = "{}/{} - ".format(self.x, self.y)
-        str_size = "{}".format(self.size)
-
-        return str_rectangle + str_id + str_xy + str_size
-
     def update(self, *args, **kwargs):
-        """ update method """
-        if args is not None and len(args) is not 0:
-            list_atr = ['id', 'size', 'x', 'y']
-            for i in range(len(args)):
-                if list_atr[i] == 'size':
-                    setattr(self, 'width', args[i])
-                    setattr(self, 'height', args[i])
-                else:
-                    setattr(self, list_atr[i], args[i])
-        else:
-            for key, value in kwargs.items():
-                if key == 'size':
-                    setattr(self, 'width', value)
-                    setattr(self, 'height', value)
-                else:
-                    setattr(self, key, value)
+        """ Update rectangle attributes. """
+        # Arg positionnel
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.size = args[1]
+        if len(args) > 2:
+            self.x = args[2]
+        if len(args) > 3:
+            self.y = args[3]
+
+        # Arg keyword
+        if 'id' in kwargs:
+            self.id = kwargs['id']
+        if 'size' in kwargs:
+            self.width = kwargs['size']
+        if 'x' in kwargs:
+            self.x = kwargs['x']
+        if 'y' in kwargs:
+            self.y = kwargs['y']
 
     def to_dictionary(self):
-        """ Returns a dictionary with attributes """
-        list_atr = ['id', 'size', 'x', 'y']
-        dict_res = {}
-
-        for key in list_atr:
-            if key == 'size':
-                dict_res[key] = getattr(self, 'width')
-            else:
-                dict_res[key] = getattr(self, key)
-
-        return dict_res
+        """ Retrun dictonary """
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
