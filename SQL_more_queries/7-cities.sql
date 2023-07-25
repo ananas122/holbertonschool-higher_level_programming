39cdea867f7e416f75c494ef66e9c368becd4284
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-25 13:30:24 +0200

    Correction

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,4 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
--- description of table data:
---      id INT unique, auto generated, can’t be null and is a primary key
---      state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states
---      name VARCHAR(256), can't be null
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
