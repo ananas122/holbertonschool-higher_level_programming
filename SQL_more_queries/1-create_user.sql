cf8d53a4c1c95ac6791b1da03a7323bfcd02adc3
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-25 10:48:50 +0200

    Correction

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- creates the MySQL server user user_0d_1 with privileges and password
-CREATE USER IF NOT EXISTS
-user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON
-*.* TO user_0d_1@localhost;
+-- create user if he doesnet exist, and grant him all perms
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
