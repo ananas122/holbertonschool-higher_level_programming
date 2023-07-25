197e5e9c390dc38ea05ed992c3863a8abd53c775
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-25 10:50:00 +0200

    Correction

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- create user if he doesnet exist, and grant him all perms
+-- create user, and grant
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
