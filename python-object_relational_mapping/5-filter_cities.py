7fe07fdbc3bf2d09f52d0b60d55b29658955407a
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-31 20:22:19 +0200

    Correction virgule

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,15 +1,31 @@
+#!/usr/bin/python3
+""" lists all cities from the database hbtn_0e_4_usa"""
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+
+    # Création d'un curseur
     cur = db.cursor()
 
-    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s", (sys.argv[4],))
+    # Exécution requêtes SQL
+    cur.execute(
+        "SELECT cities.name \
+        FROM cities \
+        JOIN states ON cities.state_id = states.id \
+        WHERE states.name = %s \
+        ORDER BY cities.id", (sys.argv[4],))
 
+    # Recuperation des résultats
     rows = cur.fetchall()
     cities = set([row[0] for row in rows])
-
     print(", ".join(cities))
 
     cur.close()
