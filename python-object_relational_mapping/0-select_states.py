e9b94c1790dfdb582bb75b532d4cbaf6ff9a417b
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-31 09:01:02 +0200

    Corrction

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa
-"""
+"""Lists all states from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
-
-
 if __name__ == '__main__':
     db = pymysql.connect(
-        user=sys.argv[1], 
-        passwd=sys.argv[2], 
+        user=sys.argv[1],
+        passwd=sys.argv[2],
         db=sys.argv[3])
 
     cur = db.cursor()
