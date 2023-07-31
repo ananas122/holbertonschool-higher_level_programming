ef47a76bd218747cac2b306e0a3ac2cb4b8b4dcb
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-31 12:17:02 +0200

    Correction ordered by states.id

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE binary 'N%' ORDER BY id ASC;")
     states = cur.fetchall()
 
     for state in states:
