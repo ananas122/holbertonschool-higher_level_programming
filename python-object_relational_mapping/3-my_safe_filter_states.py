ae8847e980dba4d064a8475c3715dafdd6ecc6f7
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-31 17:01:28 +0200

    Securité

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Write a script that takes in an argument
+and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.import MySQLdb"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+    # Création d'un curseur
+    cur = db.cursor()
+
+    # Exécution requêtes SQL
+    cur.execute(
+                "SELECT * FROM states \
+                WHERE name LIKE %s \
+                ORDER BY states.id ASC", (sys.argv[4],)
+                )
+
+    # Récupération des résultats
+    states = cur.fetchall()
+
+    # Affichage des résultats
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
