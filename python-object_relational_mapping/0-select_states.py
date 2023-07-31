5cd07e36ebe55bbb7ab49cbbaf9bf3d20ab8a2a0
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-31 08:58:59 +0200

    Corrction

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,10 +6,11 @@ import sys
 import pymysql
 
 
+
 if __name__ == '__main__':
     db = pymysql.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
+        user=sys.argv[1], 
+        passwd=sys.argv[2], 
         db=sys.argv[3])
 
     cur = db.cursor()
