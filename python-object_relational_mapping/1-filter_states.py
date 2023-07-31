4b3f6921d0a3469b9e409c0ffc19217ffbec38c1
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-31 11:58:38 +0200

    Correction

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""Lists all states  starting with 'N' from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    # Se connecter à la base de données MySQL en utilisant les arguments de la ligne de commande
     db = MySQLdb.connect(
-        host="localhost", port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3])
 
-# Créer un objet curseur pour exécuter des requêtes SQL
     cur = db.cursor()
 
-# Exécuter la requête SQL pour sélectionner toutes les lignes de la table "states"
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
-# Récupérer toutes les lignes résultantes de la requête dans une liste
     states = cur.fetchall()
 
-# Parcourir la liste des états récupérée et afficher chaque état
     for state in states:
         print(state)
 
