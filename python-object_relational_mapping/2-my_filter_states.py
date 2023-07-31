7d1dc9d5d0e919b327de6d5e8d15b49e9058fc46
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-31 16:24:11 +0200

    Supprimez les doublons

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states  starting with 'N' from the database hbtn_0e_0_usa"""
-import MySQLdb
+"""Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.import MySQLdb"""
 import sys
 
 if __name__ == '__main__':
