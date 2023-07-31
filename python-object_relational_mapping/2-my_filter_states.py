11b1881057f8b4ecb0300c4469f6cf9d53d06e24
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-31 16:05:34 +0200

    Correction

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """Lists all states  starting with 'N' from the database hbtn_0e_0_usa"""
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == '__main__':
 
     db = MySQLdb.connect(
         host="localhost", port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
 
     # Création d'un curseur
     cur = db.cursor()
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     # Exécution requêtes SQL
     cur.execute("SELECT * FROM states \
                 WHERE name LIKE '{}' \
-                ORDER BY states.id ASC".format('Arizona'))
+                ORDER BY states.id ASC".format(sys.argv[4]))
 
     # Récupération des résultats
     states = cur.fetchall()
