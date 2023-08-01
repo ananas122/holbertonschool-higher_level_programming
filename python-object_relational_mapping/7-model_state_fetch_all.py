3883c047bdac8d12a3234059fa99d3d36460aa11
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-01 10:15:22 +0200

    Correction pool pre ping

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,20 +10,21 @@ if __name__ == '__main__':
     user = sys.argv[1]
     passwd = sys.argv[2]
     db = sys.argv[3]
+    pool_pre_ping=True
 
-    # Create the engine to connect to the database
-    engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@localhost/{db}')
+    # Create the engine to connect to the db
+    engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}', pool_pre_ping=True)
 
-    # Create a session to interact with the database
+    # Create a session to interact with the db
     Session = sessionmaker(bind=engine)
 
     # create a Session
     session = Session()
 
-    # Query all State objects and sort them by id in ascending order
+    # Query all State objects and sort them by id in ASC
     states = session.query(State).order_by(State.id).all()
 
-    # Iterate through the states and print their information
+    # Iterate through the states and print
     for state in states:
         print(f"{state.id}: {state.name}")
 
