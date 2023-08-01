46aa88aa121d2a48ac7a6b404f96f0d8f97bf09e
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-01 11:32:39 +0200

    State display a

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" lists all State objects from the database hbtn_0e_6_usa"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    # Create the engine to connect to the db with pool_pre_ping=True
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}',
+        pool_pre_ping=True
+        )
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+
+    # create a Session
+    session = Session()
+
+    # Query all State objects and sort them by id in ascending order
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print(f"{state.id}: {state.name}")
+
+    session.close()
