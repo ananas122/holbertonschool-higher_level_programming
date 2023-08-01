1b7070bc854a5d8250b7d36f38f79d69e6be21da
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-01 10:20:49 +0200

    Correction create engine with argv

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ lists all State objects from the database hbtn_0e_6_usa"""
 
+
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -13,7 +14,10 @@ if __name__ == '__main__':
     pool_pre_ping=True
 
     # Create the engine to connect to the db
-    engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}', pool_pre_ping=True)
+    engine = create_engine(
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}',
+        pool_pre_ping=True,
+    )
 
     # Create a session to interact with the db
     Session = sessionmaker(bind=engine)
