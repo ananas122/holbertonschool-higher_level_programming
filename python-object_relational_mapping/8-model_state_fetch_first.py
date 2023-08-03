8aa1bebd992a52436f8c0e113b3dd30085df4ab6
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-03 11:06:38 +0200

    Correction

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""
-return all state objects from database
-"""
+""" lists all State objects from the database hbtn_0e_6_usa"""
 
-from sys import argv
+
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
 
-    # make engine
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                        format(user, passwd, db), pool_pre_ping=True)
+    # Create the engine to connect to the db with pool_pre_ping=True
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}',
+        pool_pre_ping=True
+        )
+
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
+
+    # create a Session
     session = Session()
 
-    # query python
-    firstInstance = session.query(State).order_by(State.id).first()
-    if firstInstance:
-        print("{:d}: {:s}".format(firstInstance.id, firstInstance.name))
-    else:
+    # Query all State objects and sort them by id in ascending order
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
         print("Nothing")
+    else:
+        print(f"{state.id}: {state.name}")
 
     session.close()
