94c9bbfa6b558d8b5afa491286cf13d47f54b2ce
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-03 11:43:36 +0200

    Correction

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """ lists all State objects from the database hbtn_0e_6_usa"""
 
-
-
-
-
-
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -13,14 +8,11 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
 
     # Create the engine to connect to the db with pool_pre_ping=True
-    engine = create_engine(
-        f'mysql+pymysql://{user}:{passwd}@localhost/{db}', pool_pre_ping=True
-    )
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping=True)
 
     # Create a session to interact with the db
     Session = sessionmaker(bind=engine)
