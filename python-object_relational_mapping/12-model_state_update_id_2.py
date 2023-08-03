b16359ca196611ae04dc8553bfd75f1445e8a2ca
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-03 11:48:16 +0200

    Correction

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,42 +1,22 @@
 #!/usr/bin/python3
 """
-update state: given id, change state name
-parameters given to script: username, password, database
+Changes the name of a State object from the database hbtn_0e_6_usa
 """
-
-from sys import argv
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 
-if __name__ == "__main__":
-
-    # make engine for database
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
-    engine = create_engine(
-        f'mysql+mysqldb://{user}:{passwd}@localhost/{db}', 
-        pool_pre_ping=True)
-
-    # Create a session to interact with the database
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Retrieve the State object with id = 2
-    state_id_to_update = 2
-    state_to_update = session.query(State) \
-        .filter_by(id=state_id_to_update).first()
+    stateUpdated = session.query(State).filter(State.id == 2).first()
 
-    # Check if the State object exists
-    if state_to_update is None:
-        print(f"State with id = {state_id_to_update} not found")
-    else:
-        # Update the name of the State object
-        new_name = "New Mexico"
-        state_to_update.name = new_name
+    if stateUpdated:
+        stateUpdated.name = 'New Mexico'
         session.commit()
-
-    # Close the session
-    session.close()
