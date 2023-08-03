74774c2489b1e300d91d4eff3d5aad95efc36ae2
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-03 12:22:03 +0200

    Correction

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 """
-update state: given id, change state name
-parameters given to script: username, password, database
+changes the name of a State
+object from the database hbtn_0e_6_usa
 """
 
-from sys import argv
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
+from sqlalchemy import update
 
 if __name__ == "__main__":
-
-    # make engine for database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                             format(sys.argv[1], sys.argv[2], sys.argv[3]),
                             pool_pre_ping=True)
 
-    # Créer une session pour interagir avec la base de données
+    # Initialize engine
+    Base.metadata.create_all(engine)
+
+    # Initialize session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Requête pour sup ts les objets State dont le nom contient "a"
-    for state in session.query(State):
-        if "a" in state.name:
-            session.delete(state)
-
-    # Commit des changements et fermeture de la session
+    # Update record
+    update = session.query(State).filter(State.id == 2).one()
+    update.name = "New Mexico"
     session.commit()
+
+    # Close session
     session.close()
