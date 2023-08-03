04575ca1908494abde731850af747a85df306221
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-03 12:28:32 +0200

    Correction

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,22 +10,21 @@ from model_state import State
 from model_city import City
 
 
-# make engine for database
-user = sys.argv[1]
-passwd = sys.argv[2]
-db = sys.argv[3]
-engine = create_engine(
-    f'mysql+mysqldb://{user}:{passwd}@localhost/{db}',
-    pool_pre_ping=True)
+# Make engine for database
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                        format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
 
 # Create a session to interact with the database
 Session = sessionmaker(bind=engine)
 session = Session()
 query = session.query(City, State).join(State)
 
-# Query all City objects and join with State on state_id to get state information
+# Query all City objects and join with State on state_id to get state info
 cities = session.query(City, State).join(State).order_by(City.id).all()
 
-# Iterate through the cities and print their information
+# Iterate through the cities and print their info
 for city, state in cities:
     print(f"{state.name}: ({city.id}) {city.name}")
+
