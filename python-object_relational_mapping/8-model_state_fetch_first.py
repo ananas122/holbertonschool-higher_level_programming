203cb24a2bd5b031f1e076c1179a22da8cf8b3e9
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-04 09:18:39 +0200

    Correction Pycodestyle

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,6 +3,7 @@
 return all state objects from database
 """
 
+
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -15,8 +16,9 @@ if __name__ == "__main__":
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                            format(user, passwd, db), pool_pre_ping=True)
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{passwd}@localhost/{db}', pool_pre_ping=True
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
 
