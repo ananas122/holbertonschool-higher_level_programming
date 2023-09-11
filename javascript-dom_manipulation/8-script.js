3616bc6a72fcb38cf241a879bb603bd4a3054031
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-11 16:13:31 +0200

    Translation

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,21 @@
+function fetchHelloTranslation() {
+    // Select the HTML element with id "hello"
+    const helloElement = document.getElementById('hello');
+
+    const apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+
+    // Use the Fetch API to fetch translation
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Extract and display the translation
+            const helloTranslation = data.hello;
+            helloElement.textContent = helloTranslation;
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+}
+
+// Call the function when the document is ready
+document.addEventListener('DOMContentLoaded', fetchHelloTranslation);
