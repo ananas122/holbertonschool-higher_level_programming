4c6881badb8952adfdf6543009ee0e9f8b579eb3
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-11 16:42:22 +0200

    Correction

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,23 +1,23 @@
-const movieList = document.getElementById('list_movies');
+const movieListElement = document.getElementById('list_movies');
 
-        const apiUrl = 'https://swapi-api.hbtn.io/api/films/?format=json';
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/?format=json';
 
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                // Extract and list the movie titles
-                const movies = data.results;
-                movies.forEach(movie => {
-                    // Get the title of the current movie
-                    const movieTitle = movie.title;
-                    // Create a new list item element.
-                    const listItem = document.createElement('li');
-                    // Set the text content of the list item to the movie title
-                    listItem.textContent = movieTitle;
-                    // Append the list item to the movie list element.
-                    movieListElement.appendChild(listItem);
-                });
-            })
-            .catch(error => {
-                console.error(error);
-            });
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Extract and list the movie titles
+    const movies = data.results;
+    movies.forEach(movie => {
+      // Get the title of the current movie
+      const movieTitle = movie.title;
+      // Create a new list item element.
+      const listItem = document.createElement('li');
+      // Set the text content of the list item to the movie title
+      listItem.textContent = movieTitle;
+      // Append the list item to the movie list element.
+      movieListElement.appendChild(listItem);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
