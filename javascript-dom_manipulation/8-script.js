ed5d33ec514c5940bcaf6c655eeabe3059c17ff1
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-11 17:02:30 +0200

    Correction semistandard

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,21 +1,21 @@
-function fetchHelloTranslation() {
-    // Select the HTML element with id "hello"
-    const helloElement = document.getElementById('hello');
+function fetchHelloTranslation () {
+  // Sélectionnez l'élément HTML avec l'id "hello"
+  const helloElement = document.getElementById('hello');
 
-    const apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+  const apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
 
-    // Use the Fetch API to fetch translation
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Extract and display the translation
-            const helloTranslation = data.hello;
-            helloElement.textContent = helloTranslation;
-        })
-        .catch(error => {
-            console.error('Fetch error:', error);
-        });
+  // Utilisez l'API Fetch pour récupérer la traduction
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Extrait et affiche la traduction
+      const helloTranslation = data.hello;
+      helloElement.textContent = helloTranslation;
+    })
+    .catch((error) => {
+      console.error('Erreur de fetch :', error);
+    });
 }
 
-// Call the function when the document is ready
+// Appelez la fonction lorsque le document est prêt
 document.addEventListener('DOMContentLoaded', fetchHelloTranslation);
