14d201783e1fb247263ddb277caf4b8a86c07c0c
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-11 16:35:23 +0200

    correction semistandard

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -2,7 +2,7 @@
 const updateHeaderButton = document.getElementById('update_header');
 
 // Ajoutez un gestionnaire d'événement pour le clic sur "update_header"
-updateHeaderButton.addEventListener('click', function() {
+updateHeaderButton.addEventListener('click', function () {
   // Mettez à jour le texte de l'en-tête directement
   document.querySelector('header').textContent = 'New Header!!!';
 });
